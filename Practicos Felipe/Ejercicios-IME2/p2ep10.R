a5427f7868aa096258e9f8f0e7d0421bf8bcd678
Felipe Cornejo <felipe.cornejo.i@usach.cl>
2022-01-17 21:42:57 -0300

    Practicos Felipe

---
+++ Practicos Felipe/Ejercicios-IME2/p2ep10.R
@@ -0,0 +1,6 @@
+#EP10 PREGUNTA 2
+#Prueba de suma de rangos de Wilcoxon
+#Comparar las tasas de interes de 2 bancos por mes dentro de un periodo de un año
+#Ver si las tasas se distribuyen de igual manera
+#H0: Las tasas de ambos bancos se distribuyen de igual forma
+#HA: Las tasas de ambos bancos de distribuyen de manera distinta
